cab50d2c460992981e4a90308a545b8bf0a4508f
sean <sean.g.crim@gmail.com>
2024-04-11 16:30:27 -0400

    went further through the build an app lesson

---
+++ application/in_memory_player_store.go
@@ -0,0 +1,20 @@
+package main
+
+// need to make the actual player store
+// i assume in a production setting this would be implemented as the database portion
+// TODO do this but use an actual DB
+func NewInMemoryPlayerStore() *InMemoryPlayerStore {
+	return &InMemoryPlayerStore{map[string]int{}}
+}
+
+type InMemoryPlayerStore struct {
+	store map[string]int
+}
+
+func (i *InMemoryPlayerStore) RecordWin(name string) {
+	i.store[name]++
+}
+
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return i.store[name]
+}
